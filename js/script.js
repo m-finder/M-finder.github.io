79087a574af5f902a5bdc5251fc9b764458a7bb8
wuyunfang <wuyunfang@minzhetech.com>
2018-03-08 16:02:04 +0800

    Site updated: 2018-03-08 16:02:02

--- js/script.js
+++ js/script.js
@@ -1,137 +1,109 @@
-(function($){
-  // Search
-  var $searchWrap = $('#search-form-wrap'),
-    isSearchAnim = false,
-    searchAnimDuration = 200;
+(function ($) {
+    // Search
+    var $searchWrap = $('#search-form-wrap'),
+            isSearchAnim = false,
+            searchAnimDuration = 200;
 
-  var startSearchAnim = function(){
-    isSearchAnim = true;
-  };
+    var startSearchAnim = function () {
+        isSearchAnim = true;
+    };
 
-  var stopSearchAnim = function(callback){
-    setTimeout(function(){
-      isSearchAnim = false;
-      callback && callback();
-    }, searchAnimDuration);
-  };
+    var stopSearchAnim = function (callback) {
+        setTimeout(function () {
+            isSearchAnim = false;
+            callback && callback();
+        }, searchAnimDuration);
+    };
 
-  $('#nav-search-btn').on('click', function(){
-    if (isSearchAnim) return;
+    $('#nav-search-btn').on('click', function () {
+        if (isSearchAnim)
+            return;
 
-    startSearchAnim();
-    $searchWrap.addClass('on');
-    stopSearchAnim(function(){
-      $('.search-form-input').focus();
+        startSearchAnim();
+        $searchWrap.addClass('on');
+        stopSearchAnim(function () {
+            $('.search-form-input').focus();
+        });
     });
-  });
 
-  $('.search-form-input').on('blur', function(){
-    startSearchAnim();
-    $searchWrap.removeClass('on');
-    stopSearchAnim();
-  });
+    $('.search-form-input').on('blur', function () {
+        startSearchAnim();
+        $searchWrap.removeClass('on');
+        stopSearchAnim();
+    });
 
-  // Share
-  $('body').on('click', function(){
-    $('.article-share-box.on').removeClass('on');
-  }).on('click', '.article-share-link', function(e){
-    e.stopPropagation();
+    // Caption
+    $('.article-entry').each(function (i) {
+        $(this).find('img').each(function () {
+            if ($(this).parent().hasClass('fancybox'))
+                return;
 
-    var $this = $(this),
-      url = $this.attr('data-url'),
-      encodedUrl = encodeURIComponent(url),
-      id = 'article-share-box-' + $this.attr('data-id'),
-      offset = $this.offset();
+            var alt = this.alt;
 
-    if ($('#' + id).length){
-      var box = $('#' + id);
+            if (alt)
+                $(this).after('<span class="caption">' + alt + '</span>');
 
-      if (box.hasClass('on')){
-        box.removeClass('on');
-        return;
-      }
-    } else {
-      var html = [
-        '<div id="' + id + '" class="article-share-box">',
-          '<input class="article-share-input" value="' + url + '">',
-          '<div class="article-share-links">',
-            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
-            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-            '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
-            '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
-          '</div>',
-        '</div>'
-      ].join('');
+            $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+        });
 
-      var box = $(html);
+        $(this).find('.fancybox').each(function () {
+            $(this).attr('rel', 'article' + i);
+        });
+    });
 
-      $('body').append(box);
+    if ($.fancybox) {
+        $('.fancybox').fancybox();
     }
 
-    $('.article-share-box.on').hide();
+    // Mobile nav
+    var $container = $('#container'), isMobileNavAnim = false, mobileNavAnimDuration = 200;
 
-    box.css({
-      top: offset.top + 25,
-      left: offset.left
-    }).addClass('on');
-  }).on('click', '.article-share-box', function(e){
-    e.stopPropagation();
-  }).on('click', '.article-share-box-input', function(){
-    $(this).select();
-  }).on('click', '.article-share-box-link', function(e){
-    e.preventDefault();
-    e.stopPropagation();
+    var startMobileNavAnim = function () {
+        isMobileNavAnim = true;
+    };
 
-    window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
-  });
+    var stopMobileNavAnim = function () {
+        setTimeout(function () {
+            isMobileNavAnim = false;
+        }, mobileNavAnimDuration);
+    }
 
-  // Caption
-  $('.article-entry').each(function(i){
-    $(this).find('img').each(function(){
-      if ($(this).parent().hasClass('fancybox')) return;
-
-      var alt = this.alt;
-
-      if (alt) $(this).after('<span class="caption">' + alt + '</span>');
-
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+    $('#main-nav-toggle').on('click', function () {
+        if (isMobileNavAnim) {
+            return;
+        }
+        startMobileNavAnim();
+        $container.toggleClass('mobile-nav-on');
+        stopMobileNavAnim();
     });
 
-    $(this).find('.fancybox').each(function(){
-      $(this).attr('rel', 'article' + i);
+    $('#wrap').on('click', function () {
+        if (isMobileNavAnim || !$container.hasClass('mobile-nav-on')) {
+            return;
+        }
+        $container.removeClass('mobile-nav-on');
     });
-  });
 
-  if ($.fancybox){
-    $('.fancybox').fancybox();
-  }
+    //百度统计
+    var _hmt = _hmt || [];
+    (function () {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?524d4ff49c442d1db8994544cccacfc7";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
+    
+    //百度推送
+    (function () {
+        var bp = document.createElement('script');
+        var curProtocol = window.location.protocol.split(':')[0];
+        if (curProtocol === 'https') {
+            bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+        } else {
+            bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+        }
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(bp, s);
+    })();
 
-  // Mobile nav
-  var $container = $('#container'),
-    isMobileNavAnim = false,
-    mobileNavAnimDuration = 200;
-
-  var startMobileNavAnim = function(){
-    isMobileNavAnim = true;
-  };
-
-  var stopMobileNavAnim = function(){
-    setTimeout(function(){
-      isMobileNavAnim = false;
-    }, mobileNavAnimDuration);
-  }
-
-  $('#main-nav-toggle').on('click', function(){
-    if (isMobileNavAnim) return;
-
-    startMobileNavAnim();
-    $container.toggleClass('mobile-nav-on');
-    stopMobileNavAnim();
-  });
-
-  $('#wrap').on('click', function(){
-    if (isMobileNavAnim || !$container.hasClass('mobile-nav-on')) return;
-
-    $container.removeClass('mobile-nav-on');
-  });
 })(jQuery);
