269cdac84d3a6d1465f104247bf17143c55e7699
wuyunfang <wuyunfang@minzhetech.com>
2018-03-12 14:07:04 +0800

    Site updated: 2018-03-12 14:07:00

--- js/index.js
+++ js/index.js
@@ -1,38 +1,8 @@
 layui.define(['layer', 'util', 'element'], function (exports) {
 
-    var $ = layui.jquery
-            , layer = layui.layer
-            , util = layui.util
-            , element = layui.element;
+    var $ = layui.jquery , layer = layui.layer , util = layui.util , element = layui.element;
 
-    var gather = {
-        photo: function (target) {
-            layer.photos({
-                photos: target
-                , zIndex: 9999999999
-                , anim: -1
-            });
 
-        }
-        , getDomain: function () {
-            return window.location.href.split('://')[1].split(/[/]+/, 1)[0];
-        }
-        , getQueryDir: function () {
-            return window.location.href.split('://')[1].split('?', 1)[0].replace(this.getDomain(), '').split(/\/$/, 1)[0];
-        }
-        , getUrl: function () {
-            return window.location.href.split('://')[1];
-        }
-    };
-    
-    query_dir = gather.getQueryDir();
-    
-    //导航选中
-    if (query_dir === '') {
-        $($(".nav-header-container").find("a[href='/']")).addClass('activity').siblings().removeClass('hover');
-    } else {
-        $($(".nav-header-container").find("a[href='" + query_dir + "']")).addClass('hover').siblings().removeClass('hover');
-    }
     //右下角固定Bar
     util.fixbar({
         bar1: false
@@ -43,6 +13,6 @@ layui.define(['layer', 'util', 'element'], function (exports) {
             }
         }
     });
-    
+
     exports('index');
 });
