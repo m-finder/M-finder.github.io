a934fe3ee401cf01052b61018da1f653563a3fff
wuyunfang <wuyunfang@minzhetech.com>
2018-03-23 14:14:53 +0800

    Site updated: 2018-03-23 14:14:47

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 layui.define(['layer', 'util', 'element'], function (exports) {
 
-    var $ = layui.jquery , layer = layui.layer , util = layui.util , element = layui.element;
+    var $ = layui.jquery, layer = layui.layer, util = layui.util, element = layui.element;
 
 
     //右下角固定Bar
@@ -14,5 +14,19 @@ layui.define(['layer', 'util', 'element'], function (exports) {
         }
     });
 
+    $(function () {
+        $('.dashang').click(function () {
+            layer.tab({
+                area: ['450px', '493px'],
+                tab: [{
+                    title: '<img src="/images/alipay.png">',
+                    content: '<img style="max-width:100%;height:auto;" src="/images/alipayimg.png">'
+                }, {
+                    title: '<img  src="/images/wechat.png">',
+                    content: '<img style="max-width:100%;height:auto;" src="/images/wechatimg.png">'
+                }]
+            });
+        });
+    });
     exports('index');
 });
